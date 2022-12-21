6c31347b0c98e871a620932bc4e2acf29d6bc244
Mark Baggett <mbagget1@utk.edu>
2022-12-21 10:04:14 -0500

    Add optional selection of RDF serializations.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Github, Fedora, or elsewhere.
 
 ```shell
 
-python versioner/version.py -s sheets/kintner_with_collections.csv -o output -p utk.yml
+python versioner/version.py -s sheets/kintner_with_collections.csv -o output -p utk.yml -r turtle
 
 ```
 
@@ -14,3 +14,4 @@ python versioner/version.py -s sheets/kintner_with_collections.csv -o output -p 
 * **-s**: your import sheet
 * **-o**: where to save your rdf
 * **-p**: path to your m3 profile
+* **-r**: specify your rdf serialization.  Must be one of: turtle, rdf-xml, json-ld, n3.
--- versioner/version.py
+++ versioner/version.py
@@ -7,10 +7,12 @@ import argparse
 
 
 class ImportVersioner:
-    def __init__(self, import_sheet, output_directory, profile):
+    def __init__(self, import_sheet, output_directory, profile, serialization):
         self.sheet = import_sheet
         self.output = output_directory
         self.profile = profile
+        self.serialization = self.__determine_serialization(serialization)
+        self.extension = self.__determine_extension(serialization)
         self.uri_pattern_for_subjects = "http://utk-metadata-object/"
         self.profile_as_yaml = yaml.safe_load(open(profile))['properties']
         self.lines = self.__get_lines_in_sheet(import_sheet)
@@ -49,22 +51,53 @@ class ImportVersioner:
                                     minted_object
                                 )
                             )
-                with open(f"{self.output}/{row['source_identifier']}.ttl", 'wb') as rdf:
+                with open(f"{self.output}/{row['source_identifier']}{self.extension}", 'wb') as rdf:
                     rdf.write(
-                        g.serialize(format='turtle', indent=4).encode("utf-8")
+                        g.serialize(format=self.serialization, indent=4).encode("utf-8")
                     )
         return
+
+    @staticmethod
+    def __determine_serialization(value):
+        allowed_serializations = {
+            "turtle": "turtle",
+            "json-ld": "json-ld",
+            "n3": "nt",
+            "rdf-xml": "xml"
+        }
+        return allowed_serializations[value]
+
+    @staticmethod
+    def __determine_extension(value):
+        allowed_serializations = {
+            "turtle": ".ttl",
+            "json-ld": ".json",
+            "n3": ".n3",
+            "rdf-xml": ".xml"
+        }
+        return allowed_serializations[value]
 
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='Generate RDF about a work from a Bulkrax Import CSV.')
+    parser = argparse.ArgumentParser(
+        prog="Bulkrax Import RDF Generator",
+        description='Generate RDF about a work from a Bulkrax Import CSV.'
+    )
     parser.add_argument("-s", "--sheet", dest="sheet", help="Path to your importer CSV", required=True)
     parser.add_argument("-p", "--profile", dest="profile", help="Path to m3 profile.", required=True)
     parser.add_argument("-o", "--output", dest="output", help="Path to where to save your rdf files.", required=True)
+    parser.add_argument(
+        "-r", "--rdf_serialization",
+        dest="rdf_serialization",
+        help="Specify your rdf serialization.",
+        default="turtle",
+        choices=['turtle', 'json-ld', 'n3', 'rdf-xml']
+    )
     args = parser.parse_args()
     x = ImportVersioner(
         import_sheet=args.sheet,
         output_directory=args.output,
-        profile=args.profile
+        profile=args.profile,
+        serialization=args.rdf_serialization
     )
